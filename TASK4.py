155e07aa7ec738ee2bfdd880fafa6f1adcfdb0cb
Avin Madhu <84233047+avin-madhu@users.noreply.github.com>
2024-07-15 12:51:06 +0530

    Update TASK4.py

--- TASK4.py
+++ TASK4.py
@@ -31,7 +31,7 @@ choice_dict = {1: "Rock", 2: "Paper", 3: "Scissor"}
 name = input("\nEnter your player name: ")
 
 while (1):
-    print(f"------- ROUND {round} -----------")
+    print(f"\n------- ROUND {round} -----------")
     print("1) Rock : 1\n2) Paper: 2\n3) Scissor: 3")
     user_choice = int(input("\nEnter your choice: "))
 
@@ -39,16 +39,35 @@ while (1):
     while (str(user_choice) not in "123"):
         user_choice = int(input("\nEnter a valid choice: "))
 
-    if (user_choice == 1):
-        computer_choice = random.randint(1,3)
-        if (user_choice - computer_choice):
-            print("\nIts's a tie")
-            print(f" {name}: {user_score} || Computer: {computer_score}")
+    computer_choice = random.randint(1,3)
+    print("computer choice: ", computer_choice)
+    if (user_choice - computer_choice)==0:
+        print(f"\ncomputer: {choice_dict[computer_choice]} || {name}: {choice_dict[user_choice]}")
+        print("\nIts's a tie")
+        print(f"\n{name}: {user_score} || Computer: {computer_score}")
+        print("\nWant to go for another round? (y/n): ")
+        again = input()
+
+        # if the user give somthing except y or n
+        while(again != "y" and again != "n"):
+            print("\nEnter a valid answer (y/n): ")
+            again = input()
+        if again == "y":
+            round += 1
+            continue
+        else:
+            break
+    else:
+        if (user_choice==1 and computer_choice==2):
+            print(f"\ncomputer: {choice_dict[computer_choice]} || {name}: {choice_dict[user_choice]}")
+            print("\nComputer Scores 1 point")
+            computer_score+=1
+            print(f"\n{name}: {user_score} || Computer: {computer_score}")
             print("\nWant to go for another round? (y/n): ")
             again = input()
 
             # if the user give somthing except y or n
-            while(again != "y" or again != "n"):
+            while(again != "y" and again != "n"):
                 print("\nEnter a valid answer (y/n): ")
                 again = input()
             if again == "y":
@@ -56,10 +75,88 @@ while (1):
                 continue
             else:
                 break
-        else:
-            pass
-            
+        elif (user_choice==2 and computer_choice==1):
+            print(f"\ncomputer: {choice_dict[computer_choice]} || {name}: {choice_dict[user_choice]}")
+            print("\nComputer scores 1 point")
+            computer_score+=1
+            print(f"\n{name}: {user_score} || Computer: {computer_score}")
+            print("\nWant to go for another round? (y/n): ")
+            again = input()
 
+            # if the user give somthing except y or n
+            while(again != "y" and again != "n"):
+                print("\nEnter a valid answer (y/n): ")
+                again = input()
+            if again == "y":
+                round += 1
+                continue
+            else:
+                break
+        elif (user_choice==1 and computer_choice == 3):
+            print(f"\ncomputer: {choice_dict[computer_choice]} || {name}: {choice_dict[user_choice]}")
+            print(f"\n{name} scores 1 point")
+            user_score+=1
+            print(f"\n{name}: {user_score} || Computer: {computer_score}")
+            print("\nWant to go for another round? (y/n): ")
+            again = input()
 
+            # if the user give somthing except y or n
+            while(again != "y" and again != "n"):
+                print("\nEnter a valid answer (y/n): ")
+                again = input()
+            if again == "y":
+                round += 1
+                continue
+            else:
+                break
+        elif (user_choice==2 and computer_choice == 3):
+            print(f"\ncomputer: {choice_dict[computer_choice]} || {name}: {choice_dict[user_choice]}")
+            print("\nComputer scores 1 point")
+            computer_score+=1
+            print(f"\n{name}: {user_score} || Computer: {computer_score}")
+            print("\nWant to go for another round? (y/n): ")
+            again = input()
 
+            # if the user give somthing except y or n
+            while(again != "y" and again != "n"):
+                print("\nEnter a valid answer (y/n): ")
+                again = input()
+            if again == "y":
+                round += 1
+                continue
+            else:
+                break
+        elif (user_choice==3 and computer_choice == 1):
+            print(f"\ncomputer: {choice_dict[computer_choice]} || {name}: {choice_dict[user_choice]}")
+            print("\nComputer scores 1 point")
+            computer_score+=1
+            print(f"\n{name}: {user_score} || Computer: {computer_score}")
+            print("\nWant to go for another round? (y/n): ")
+            again = input()
 
+            # if the user give somthing except y or n
+            while(again != "y" and again != "n"):
+                print("\nEnter a valid answer (y/n): ")
+                again = input()
+            if again == "y":
+                round += 1
+                continue
+            else:
+                break
+        elif (user_choice==3 and computer_choice == 2):
+            print(f"\ncomputer: {choice_dict[computer_choice]} || {name}: {choice_dict[user_choice]}")
+            print(f"\n{name} scores 1 point")
+            user_score+=1
+            print(f"\n{name}: {user_score} || Computer: {computer_score}")
+            print("\nWant to go for another round? (y/n): ")
+            again = input()
+
+            # if the user give somthing except y or n
+            while(again != "y" and again != "n"):
+                print("\nEnter a valid answer (y/n): ")
+                again = input()
+            if again == "y":
+                round += 1
+                continue
+            else:
+                break
